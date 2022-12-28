7eeb2181e0b095adf5ef8cae0fb578f527def6cf
Phalguna_DevOps <devOps.phalguna@gmail.com>
2022-12-28 13:21:26 +0530

    test cases are added

---
+++ tests/test_division.py
@@ -0,0 +1,12 @@
+from src.calculator import divide
+import pytest
+
+
+def test_divide():
+    result = divide(3, 2)
+    assert result == 1.5
+
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError) as e:
+        divide(3, 0)
